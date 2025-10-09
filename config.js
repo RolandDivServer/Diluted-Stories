1c77619d44e99a3107c6b612a0c65576fdcb185e
RolandDivServer <rolandjoycorreia@gmail.com>
2025-10-09 23:25:58 +0100

    Update config.js
    set up of contact forms end point

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
    (e.g., Formspree, Basin, Getform, Netlify Forms, a Cloudflare Worker, or a Google Apps Script).
    DO NOT put your raw email in the HTML—keep it server-side at the provider.
 */
-window.FORM_ENDPOINT = 'https://YOUR-FORM-ENDPOINT-HERE';
+window.FORM_ENDPOINT = 'https://formspree.io/f/mblzzlvj';
